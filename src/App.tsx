fa500e00b7305b72a069ea1777b8bde15dfb05eb
MarcinTomkiewicz <marc.tomk@gmail.com>
2023-06-19 16:34:55 +0200

    Responsive version of menu for larger screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,23 @@
-import logo from "../public/logo.png";
+import { TopMenu } from "./components/TopMenu/TopMenu";
+import { useState } from "react";
 
 function App() {
 	return (
-		<div className="wrapper">
-			<div className="top__menu">
-				<img src={process.env.PUBLIC_URL + "/logo.png"} style={{ height: "100%" }}></img>
-				<div className="menu__items">
-					<div className="menu__element--wrapper">
-						<div className="menu__element">Home</div>
-					</div>
-					<div className="menu__element--wrapper">
-						<div className="menu__element">O Kompanii</div>
-					</div>
-					<div className="menu__element--wrapper">
-						<div className="menu__element">Galeria</div>
-					</div>
-					<div className="menu__element--wrapper">
-						<div className="menu__element">Zloty</div>
-					</div>
-					<div className="menu__element--wrapper">
-						<div className="menu__element">Kontakt</div>
-					</div>
+		<>
+			<div className="wrapper">
+				<TopMenu />
+				<img src={process.env.PUBLIC_URL + "/kompania.jpg"} style={{ maxWidth: "85%", maxHeight: "600px", borderRadius: "15px" }} />
+				<div className="main__text">
+					Jesteśmy grupą przyjaciół, których łączy zamiłowanie do spotkań o charakterze militarnym. Nie wzięło się to znikąd. Część z nas ukończyła
+					szkoły o profilu wojskowym i policyjnym, niektórzy postanowili kontynuować karierę w tym kierunku a część osób po prostu to lubi. Dlatego
+					spotykamy się na różnego rodzaju zlotach militarnych i organizujemy spotkania sami. Kolekcjonujemy sprzęt militarny w którym lubimy się
+					pokazać.
+				</div>
+				<div className="main__text" style={{ color: "#1a2d00", fontSize: "30px", fontWeight: "bold" }}>
+					"Nie możemy Cię zmusić, żebyś coś zrobił, ale możemy sprawić, że będziesz żałował, że tego nie zrobiłeś z nami..."
 				</div>
 			</div>
-			<img src={process.env.PUBLIC_URL + "/kompania.jpg"} style={{ width: "85%", borderRadius: "15px" }} />
-			<div className="main__text">
-				Jesteśmy grupą przyjaciół, których łączy zamiłowanie do spotkań o charakterze militarnym. Nie wzięło się to znikąd. Część z nas ukończyła szkoły
-				o profilu wojskowym i policyjnym, niektórzy postanowili kontynuować karierę w tym kierunku a część osób po prostu to lubi. Dlatego spotykamy się
-				na różnego rodzaju zlotach militarnych i organizujemy spotkania sami. Kolekcjonujemy sprzęt militarny w którym lubimy się pokazać.
-			</div>
-      <div className="main__text" style={{color: "#1a2d00", fontSize: "30px", fontWeight: "bold"}}>"Nie możemy Cię zmusić, żebyś coś zrobił, ale możemy sprawić, że będziesz żałował, że tego nie zrobiłeś z nami..."</div>
-		</div>
+		</>
 	);
 }
 
---
+++ src/components/TopMenu/TopMenu.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { Offcanvas } from "react-bootstrap";
+
+export const TopMenu = () => {
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
+	const menuElements: string[] = ["Home", "O Kompanii", "Galeria", "Zloty", "Kontakt"];
+
+	return (
+		<>
+			<div className="top__menu">
+				<img src={process.env.PUBLIC_URL + "/logo.png"} style={{ height: "100%" }}></img>
+				<div className="menu__items">
+					{menuElements.map((menuElement) => {
+						return (
+							<div className="menu__element--wrapper">
+								<div className="menu__element">{menuElement}</div>
+							</div>
+						);
+					})}
+				</div>
+				<div className={!show ? "hamburger__menu" : "hamburger__menu open"} onClick={handleShow}>
+					<div className="icon-E"></div>
+					<div className="icon-X"></div>
+				</div>
+				<Offcanvas show={show} onHide={handleClose} style={{ backgroundColor: "black", color: "azure" }}>
+					<Offcanvas.Header closeButton>
+						<Offcanvas.Title>Kompania Fenix</Offcanvas.Title>
+					</Offcanvas.Header>
+					<Offcanvas.Body>
+						
+							{menuElements.map((menuElement) => {
+								return (
+									<div className="d-flex flex-column justify-content-start align-items-start w-100">
+										<div className="menu__element">{menuElement}</div>
+									</div>
+								);
+							})}
+						{/* </div> */}
+					</Offcanvas.Body>
+				</Offcanvas>
+			</div>
+		</>
+	);
+};
