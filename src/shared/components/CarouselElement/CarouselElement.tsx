c1bd40359a8f9a8976125497979f2f93e07eb762
MarcinTomkiewicz <marc.tomk@gmail.com>
2023-07-31 13:35:50 +0200

    Removed failing code

--- src/shared/components/CarouselElement/CarouselElement.tsx
+++ src/shared/components/CarouselElement/CarouselElement.tsx
@@ -3,33 +3,16 @@ import { Button } from "react-bootstrap";
 import { CarouselElementPropsInterface } from "../../utils/interfaces";
 import { Link } from "react-router-dom";
 import React from "react";
-import Style from "style-it";
 import "./carouselElement.css";
 
 export const CarouselElement = ({ imgSource, header, description, buttonText, buttonLink, style }: CarouselElementPropsInterface): React.ReactElement => {
-	// console.log((1660 - window.innerWidth).toString());
 	return (
 		<>
-			{/* <Style>
-				{`.animated__wrapper::before {
-					content: "";
-					position: absolute;
-					top: 0; 
-					left: 0;
-					width: 100%;
-					height: 100%;
-					// background-image: url(${process.env.PUBLIC_URL + imgSource});
-					filter: grayscale(100%);
-					background-repeat: no-repeat;
-					background-size: cover;
-				}
-			}`}
-			</Style> */}
 			<animated.div
 				className="animated__wrapper"
 				style={{
 					...style,
-					backgroundImage: `url(${process.env.PUBLIC_URL + imgSource})`
+					backgroundImage: `url(${process.env.PUBLIC_URL + imgSource})`,
 				}}
 			>
 				<div className="element__wrapper">
